088addce8a32348734c826fc2a10f365cd55ddd6
deelorr <deelorr@gmail.com>
2024-02-12 05:24:18 -0800

    fixed db issue

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
